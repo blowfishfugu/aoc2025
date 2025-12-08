66226cd9fbc0cabe1820794a121181adf2673bce
blowfishfugu <coolm12345@hotmail.de>
2025-12-08 15:51:47 +0100

    D08, added comments

--- D08/main.cpp
+++ D08/main.cpp
@@ -12,6 +12,7 @@ using I = std::int64_t;
 
 void run1(const fs::path& inputFile)
 {
+	//yet another vec-xyz-implementation
 	struct Vec3 {
 		std::tuple<I, I, I> data{};
 		//named accessors
@@ -31,30 +32,39 @@ void run1(const fs::path& inputFile)
 				}
 			}
 		}
-
+		
+		//math needed for puzzle
+		
+		//direction of vec3s
 		Vec3 operator-(const Vec3& r) const {
 			return { {r.x() - x(), r.y() - y(), r.z() - z()} };
 		}
 
+		//euclidian distance unsquared, exact length not needed
 		I sqLen() const {
 			return x() * x() + y() * y() + z() * z();
 		}
 	};
+
+	//Items to read, have a position, and a circuit-ID
 	struct Junction {
 		Vec3 pos{};
 		size_t circuitID{};
-		explicit Junction(std::string_view toParse) noexcept
+		explicit Junction(std::string_view toParse /* x,y,z */ ) noexcept
 		{
-			using CommaIter = SplitIter<','>;
-			auto it = CommaIter{ toParse };
-			for (size_t i = 0; i < 3 && it != CommaIter{};++i, ++it) {
-				std::from_chars((*it).data(), (*it).data() + (*it).size(), pos[i]);
+			using CommaSplitter = SplitIter<','>;
+			auto current = CommaSplitter{ toParse };
+			auto end = CommaSplitter{};
+			for (size_t i = 0; i < 3 && current != end;++i, ++current) {
+				const auto& sv = *current;
+				std::from_chars(sv.data(), sv.data()+sv.size(), pos[i]);
 			}
 		}
 	};
 
 	using Junctions = std::vector<Junction>;
 	
+	//reading positions
 	TxtFile txt(inputFile);
 	Junctions juncs;
 	for ( const auto& line : txt)
@@ -62,6 +72,7 @@ void run1(const fs::path& inputFile)
 		if (line.size() == 0) { break; }
 		juncs.emplace_back(line);
 	}
+
 	//example 20 juncs and limit 10
 	//in puzzle 1000 juncs and limit=1000!
 	int cap = (juncs.size() == 20) ? 10 : 1000;
@@ -70,6 +81,7 @@ void run1(const fs::path& inputFile)
 	using Distance = std::tuple<I, Vec3, Junction*, Junction*>;
 	using Distances = std::vector<Distance>;
 	Distances dists{};
+	//pairing each-by-remaining
 	for (size_t i = 0; i < juncs.size(); ++i) {
 		Junction* current = &(juncs[i]);
 		for (size_t j = i + 1; j < juncs.size(); ++j) {
@@ -80,6 +92,7 @@ void run1(const fs::path& inputFile)
 		}
 	}
 
+	//sort-by-lengths
 	std::sort(dists.begin(), dists.end(), [](const auto& l, const auto& r)
 		{
 			return std::get<0>(l) < std::get<0>(r);
@@ -91,35 +104,36 @@ void run1(const fs::path& inputFile)
 
 	for (int i = 0; Distance& d:dists) {
 		auto& [len, dir, j1, j2] = d;
-		if (j1->circuitID == 0 && j2->circuitID == 0) {
+		if (j1->circuitID == 0 && j2->circuitID == 0) { //both unassigned, new circuit
 			circuits.emplace_back(Circuit{});
 			j1->circuitID = circuits.size();
 			j2->circuitID = circuits.size();
 			circuits[j1->circuitID - 1].emplace_back(j1);
 			circuits[j2->circuitID - 1].emplace_back(j2);
 		}
-		else if (j1->circuitID == 0) {
+		else if (j1->circuitID == 0) { //attach junc1 to junc2s circuit
 			j1->circuitID = j2->circuitID;
 			Circuit& c1 = circuits[j1->circuitID - 1];
 			c1.emplace_back(j1);
-			if (c1.size() == juncs.size()) {
+			if (c1.size() == juncs.size()) { //all juncs are in one circuit
 				std::println(std::cout, "last connection {} x {} = {}",
 					j1->pos.x(), j2->pos.x(), j1->pos.x() * j2->pos.x());
 				break;
 			}
 		}
-		else if (j2->circuitID == 0) {
+		else if (j2->circuitID == 0) { //attach junc2 to junc1s circuit
 			j2->circuitID = j1->circuitID;
 			Circuit& c2 = circuits[j2->circuitID - 1];
 			c2.emplace_back(j2);
-			if (c2.size() == juncs.size()) {
+			if (c2.size() == juncs.size()) { //all juncs are in one circuit
 				std::println(std::cout, "last connection {} x {} = {}",
 					j1->pos.x(), j2->pos.x(), j1->pos.x()* j2->pos.x());
 				break;
 			}
 		}
 		else {
-			if (j1->circuitID != j2->circuitID) {
+			//both in same circuit, nothing happens, different circuits->join.
+			if (j1->circuitID != j2->circuitID) { //otherwise merge circuits
 				//std::println(std::cout, "merging circuit {} and {} ?", j1->circuitID, j2->circuitID);
 				Circuit& c1 = circuits[j1->circuitID- 1];
 				Circuit& c2 = circuits[j2->circuitID- 1];
@@ -129,7 +143,8 @@ void run1(const fs::path& inputFile)
 					c1.emplace_back(j);
 				}
 				c2.clear();
-				if (c1.size() == juncs.size()) {
+
+				if (c1.size() == juncs.size()) { //all juncs are in one circuit
 					std::println(std::cout, "last connection {} x {} = {}",
 						j1->pos.x(), j2->pos.x(), j1->pos.x()* j2->pos.x());
 					break;
@@ -137,14 +152,12 @@ void run1(const fs::path& inputFile)
 			}
 		}
 
-
 		++i;
-		if (i == cap) {
+		if (i == cap) { //on first attempt (part2), sorted here, and made a mess
 			for (Circuit& c : circuits) { sizesOnCap.emplace_back(c.size()); }
 		}
 	}
-	
-	
+
 	//result of part1
 	std::sort(sizesOnCap.begin(), sizesOnCap.end(), [](const size_t& l, const size_t& r) {return l > r; });
 
